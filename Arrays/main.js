ce40333adfcc17097eb3674e7612a2ccad239934
Denis Moini <denismoini09@gmail.com>
2022-10-02 12:22:25 +0300

    first commit

---
+++ Arrays/main.js
@@ -0,0 +1,25 @@
+/* Arrays are special variables which can hold more than one element
+of different data types
+ */
+// creating an empty array
+const arr = []
+
+// creating an empty array with the keyword new
+const array1 = new Array()
+
+// creating an array with predefined elements
+const colors = ['red', 'yellow', 'green']
+
+// creating an array with the keyword new with predefined elements
+const array2 = new Array('red', 'yellow', 'green')
+
+//Elements in an array can be accessed through zero indexing
+colors[0] = 'red'
+colors[1] = 'yellow'
+colors[2] = 'green'
+
+
+//Array with elements of different data types
+const mixedArray = [1, 'boy', {a: 3}]
+
+
